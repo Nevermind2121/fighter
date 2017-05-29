86b5fcf16d060d40cb625e6553a683f15128e849
Gorceag Mihail <mihail.gorceag@yopeso.com>
2017-05-29 16:12:33 +0300

    finish implement step method

--- fight.js
+++ fight.js
@@ -23,22 +23,22 @@ class Person {
     this.leftHand = {x1: x, y1: y+r*2, x2: x-r*1.5, y2: y+r*3, x3: x-r*1.5, y3: y+r*4.5}
     this.rightHand = {x1: x, y1: y+r*2, x2: x+r*1.5, y2: y+r*3, x3: x+r*3, y3: y+r*3.5}
     this.body = {x1: x, y1: y+r*2, x2: x, y2:y+r*6}
-    this.leftLeg = {x1: x, y1:y+r*6, x2: x-r*0.75, y2: y+r*7.5, x3: x-r*1.5, y3: y+r*9, groupX: x-r*1.5, groupY: y+r*6}
-    this.rightLeg = {x1: x, y1:y+r*6, x2: x+r*0.75, y2: y+r*7.5, x3: x+r*1.5, y3: y+r*9}
+    this.leftLeg = {x1: 22.5, y1:0, x2: 11.25, y2: 22.5, x3: 0, y3: 45, groupX: x-r*1.5, groupY: y+r*6}
+    this.rightLeg = {x1: 0, y1:0, x2: 11.25, y2: 22.5, x3: 22.5, y3: 45, groupX: x, groupY: y+r*6}
     this.svgCanvas = document.getElementById(svg)
     this.enemy = enemy
   }
 
-  changeAllXAxis(value) {
-    this.x = this.x + value
-    this.head = Object.assign(this.head, {x: this.x})
-    this.neck = Object.assign(this.neck, {x1: this.x, x2: this.x})
-    this.leftHand = Object.assign(this.leftHand, {x1: this.x, x2: this.x-this.r*1.5, x3: this.x-this.r*1.5})
-    this.rightHand = Object.assign(this.rightHand, {x1: this.x, x2: this.x+this.r*1.5, x3: this.x+this.r*3})
-    this.body = Object.assign(this.body, {x1: this.x, x2: this.x})
-    this.leftLeg = Object.assign(this.leftLeg, {groupX: this.x-this.r*1.5})
-    this.rightLeg = Object.assign(this.rightLeg, {x1: this.x, x2: this.x+this.r*0.75, x3: this.x+this.r*1.5})
-  }
+  // changeAllXAxis(value) {
+  //   this.x = this.x + value
+  //   this.head = Object.assign(this.head, {x: this.x})
+  //   this.neck = Object.assign(this.neck, {x1: this.x, x2: this.x})
+  //   this.leftHand = Object.assign(this.leftHand, {x1: this.x, x2: this.x-this.r*1.5, x3: this.x-this.r*1.5})
+  //   this.rightHand = Object.assign(this.rightHand, {x1: this.x, x2: this.x+this.r*1.5, x3: this.x+this.r*3})
+  //   this.body = Object.assign(this.body, {x1: this.x, x2: this.x})
+  //   this.leftLeg = Object.assign(this.leftLeg, {groupX: this.x-this.r*1.5})
+  //   this.rightLeg = Object.assign(this.rightLeg, {x1: this.x, x2: this.x+this.r*0.75, x3: this.x+this.r*1.5})
+  // }
 
   // animation(value, animationDuration, animatedMovement) {
   //   let progress = 0;
@@ -73,25 +73,17 @@ class Person {
 
   move(direction) {
     const currX = this.x
-    if (direction === 'left') {
-      const targetX = this.x - 10
+    let targetX = null
+    if (direction === 'left') targetX = currX - 10
+    else if (direction === 'right') targetX = currX + 10
 
-      let interv = setInterval(()=> {
-        this.changeAllXAxis(-1)
-        this.render()
-        if (this.x === targetX) clearInterval(interv)
-      }, 10)
-    }
-    else if (direction === 'right') {
-      const targetX = this.x + 10
-
-      let interv = setInterval(()=> {
-        this.changeAllXAxis(1)
-        this.step()
-        this.render()
-        if (this.x === targetX) clearInterval(interv)
-      }, 10)
-    }
+    let interv = setInterval(()=> {
+      if (direction === 'left') this.x = this.x - 1
+      else if (direction === 'right') this.x = this.x + 1
+      this.render()
+      if (this.x === targetX) clearInterval(interv)
+    }, 10)
+    this.step()
   }
 
   step() {
@@ -100,32 +92,40 @@ class Person {
     const diffBetweenStartAndEndRightLeg = this.rightLeg.x1 - this.rightLeg.x2
     const diffBetweenStartAndEnd2RightLeg = this.rightLeg.x1 - this.rightLeg.x3
 
-    const diffBetweenStartAndEndLeftLeg = this.leftLeg.x2 - this.rightLeg.x2
-    const diffBetweenStartAndEnd2LeftLeg = this.leftLeg.x3 - this.rightLeg.x3
-      
-    if (back) {
-      this.leftLeg.x2 = this.leftLeg.x2 + diffBetweenStartAndEndLeftLeg/5
-      this.leftLeg.x3 = this.leftLeg.x3 + diffBetweenStartAndEnd2LeftLeg/5
+    const diffBetweenStartAndEndLeftLeg = this.leftLeg.x2 + this.rightLeg.x2
+    const diffBetweenStartAndEnd2LeftLeg = this.leftLeg.x1 + this.rightLeg.x3
+    
+    const startPositionRightLegX1 = 0
+    const startPositionRightLegX2 = 11.25
+    const startPositionRightLegX3 = 22.5
 
-      this.rightLeg.x2 = this.rightLeg.x2 - diffBetweenStartAndEndRightLeg/5
-      this.rightLeg.x3 = this.rightLeg.x3 - diffBetweenStartAndEnd2RightLeg/5
-      if (this.rightLeg.x2 === this.x+this.r*0.75 && this.rightLeg.x3 === this.x+this.r*1.5) clearInterval(interv)
-    }
-    else {
-      this.leftLeg.x2 = this.leftLeg.x2 - diffBetweenStartAndEndLeftLeg/5
-      this.leftLeg.x3 = this.leftLeg.x3 - diffBetweenStartAndEnd2LeftLeg/5
+    const startPositionLeftLegX1 = 22.5
+    const startPositionLeftLegX2 = 11.25
+    const startPositionLeftLegX3 = 0
 
-      this.rightLeg.x2 = this.rightLeg.x2 + diffBetweenStartAndEndRightLeg/5
-      this.rightLeg.x3 = this.rightLeg.x3 + diffBetweenStartAndEnd2RightLeg/5
-      if (this.rightLeg.x2 === this.rightLeg.x1 && this.rightLeg.x3 === this.rightLeg.x1) back = true
-    }
+    let interv = setInterval(()=> {
+      if (back) {
+        this.leftLeg.x2 = this.leftLeg.x2 - diffBetweenStartAndEndLeftLeg/5
+        this.leftLeg.x3 = this.leftLeg.x3 - diffBetweenStartAndEnd2LeftLeg/5
+
+        this.rightLeg.x2 = this.rightLeg.x2 - diffBetweenStartAndEndRightLeg/5
+        this.rightLeg.x3 = this.rightLeg.x3 - diffBetweenStartAndEnd2RightLeg/5
+        if (this.rightLeg.x2 === startPositionRightLegX2 && this.rightLeg.x3 === startPositionRightLegX3) clearInterval(interv)
+      }
+      else {
+        this.leftLeg.x2 = this.leftLeg.x2 + diffBetweenStartAndEndLeftLeg/5
+        this.leftLeg.x3 = this.leftLeg.x3 + diffBetweenStartAndEnd2LeftLeg/5
+
+        this.rightLeg.x2 = this.rightLeg.x2 + diffBetweenStartAndEndRightLeg/5
+        this.rightLeg.x3 = this.rightLeg.x3 + diffBetweenStartAndEnd2RightLeg/5
+        if (this.rightLeg.x2 === this.rightLeg.x1 && this.rightLeg.x3 === this.rightLeg.x1) back = true
+      }
+      this.render()
+    }, 10)
+    
   }
 
   render() {
-    this.recountCoords(this.leftLeg.x1, this.leftLeg.y1, this.rightHand)
-    this.recountCoords(this.leftLeg.x3, this.leftLeg.y1, this.leftLeg)
-    this.recountCoords(this.rightLeg.x1, this.rightLeg.y1, this.rightLeg)
-    console.log('render fn')
     const svg = this.svgCanvas
     const person = []
     const head = `<circle cx="${this.head.x}" cy="${this.head.y}" r="${this.head.r}" class="head"/>`
@@ -135,35 +135,31 @@ class Person {
     <line x1="${this.leftHand.x1}" y1="${this.leftHand.y1}" x2="${this.leftHand.x2}" y2="${this.leftHand.y2}" stroke="black" stroke-width="2"/>
     <line x1="${this.leftHand.x2}" y1="${this.leftHand.y2}" x2="${this.leftHand.x3}" y2="${this.leftHand.y3}" stroke="black" stroke-width="2"/>
     `
-    const rightHand = ` <g class="rightHand" transform="translate(${this.rightHand.minX}, ${this.rightHand.minY})">
+    const rightHand = ` <g class="rightHand">
       <line x1="${this.rightHand.x1}" y1="${this.rightHand.y1}" x2="${this.rightHand.x2}" y2="${this.rightHand.y2}" stroke="black" stroke-width="2"/>
       <line x1="${this.rightHand.x2}" y1="${this.rightHand.y2}" x2="${this.rightHand.x3}" y2="${this.rightHand.y3}" stroke="black" stroke-width="2"/>
     </g>`
     
-    const leftLeg = `<g class="leftleg" transform="translate(${this.leftLeg.minX}, ${this.leftLeg.minY})" >
+
+    let legs = 
+    `<g class="leftleg" transform="translate(${this.leftLeg.groupX}, ${this.leftLeg.groupY})" >
       <line x1="${this.leftLeg.x1}" y1="${this.leftLeg.y1}" x2="${this.leftLeg.x2}" y2="${this.leftLeg.y2}" stroke="black" stroke-width="2"/>
       <line x1="${this.leftLeg.x2}" y1="${this.leftLeg.y2}" x2="${this.leftLeg.x3}" y2="${this.leftLeg.y3}" stroke="black" stroke-width="2"/>
-    <g/>`
-
-    let rightLeg = `<g class="rightLeg" transform="translate(${this.rightLeg.minX}, ${this.rightLeg.minY})">
+    </g>
+    <g class="rightLeg" transform="translate(${this.rightLeg.groupX}, ${this.rightLeg.groupY})">
       <line x1="${this.rightLeg.x1}" y1="${this.rightLeg.y1}" x2="${this.rightLeg.x2}" y2="${this.rightLeg.y2}" stroke="black" stroke-width="2"/>
       <line x1="${this.rightLeg.x2}" y1="${this.rightLeg.y2}" x2="${this.rightLeg.x3}" y2="${this.rightLeg.y3}" stroke="black" stroke-width="2"/>
     </g>`
-    const legs = `${leftLeg}${rightLeg}`
+
     person.push(head, neck, body, leftHand, rightHand, legs)
-
-
 
     let isEnemy = ''
     if (this.enemy) isEnemy = 'enemy'
 
-    const svgPerson = `<g class="person ${isEnemy}">` + person.join('') + '</g>'
-    console.log(svgPerson)
+    const svgPerson = `<g transform="translate(${this.x})" class="person ${isEnemy}">` + person.join('') + '</g>'
     svg.innerHTML = svgPerson
 
     const allGroupElems = document.getElementsByTagName('g')
-    console.log('allGroupElems')
-    console.log(allGroupElems)
   }
 }
 
